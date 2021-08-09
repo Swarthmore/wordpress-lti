a526eddd1b23d25ab91e5abd0957fe7772f66929
Anthony Weed <aweed1@swarthmore.edu>
2021-08-09 14:45:32 -0400

    Actually removed vendor/ from .gitignore this time.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '2.2.0',
+        'version' => '2.2.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'celtic/wordpress-lti',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'celtic/lti' => array(
+            'pretty_version' => 'v4.4.1',
+            'version' => '4.4.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../celtic/lti',
+            'aliases' => array(),
+            'reference' => 'b2c675187597c3bf2b6bdc82c5f549ea1400ca22',
+            'dev_requirement' => false,
+        ),
+        'celtic/wordpress-lti' => array(
+            'pretty_version' => '2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v5.4.0',
+            'version' => '5.4.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'd2113d9b2e0e349796e72d2a63cf9319100382d2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
